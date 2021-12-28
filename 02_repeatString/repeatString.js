00ac47f49d817144582e988fba3c4ddfee50c6ee
amit25095 <amit25095@gmail.com>
2021-12-28 19:33:11 +0200

    finished 01 - 07 js excercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (str, timesToRepeat) {
+    let res = '';
 
+    if (timesToRepeat < 0)
+        return "ERROR";
+
+    for (let i = 0; i < timesToRepeat; i++)
+        res += str;
+
+    return res;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    let sum = 0;
+    let low = Math.min(start, end), high = Math.max(start, end);
 
+    if (![...arguments].every(elem => elem >= 0 && typeof elem == 'number'))
+        return 'ERROR';
+
+    for (let i = low; i <= high; i++)
+        sum += i;
+    return sum;
 };
 
 // Do not edit below this line
