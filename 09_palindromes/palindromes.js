cea969beeb91e7c378ab5750bfb4c89f202c9f35
amit25095 <amit25095@gmail.com>
2022-03-28 18:52:46 +0300

    finished exc 9-11

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    s = s.toLowerCase().replace(/[^a-z]/g, "");
+    return s.split("").reverse().join("") === s;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+    index = Number(index);
+    if (index <= 0) return 'OOPS';
+    if (index === 1 || index === 2) return 1;
 
+    return fibonacci(index - 1) + fibonacci(index - 2);
 };
 
 // Do not edit below this line
