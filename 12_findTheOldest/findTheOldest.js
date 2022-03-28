6e8971396696c102b9425c62d17cd2fe08debf7e
amit25095 <amit25095@gmail.com>
2022-03-28 19:24:03 +0300

    finished finTheOldest excercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    return people.reduce((oldest, current) => {
+        if (!oldest) return current;
+        return getAge(oldest) < getAge(current) ? current : oldest;
+    });
 };
+
+const getAge = (person) => ((person.yearOfDeath ?? new Date().getFullYear()) - person.yearOfBirth);
 
 // Do not edit below this line
 module.exports = findTheOldest;
